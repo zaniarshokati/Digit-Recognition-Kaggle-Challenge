c89f4d1dc8fe796315ddbce7b1bdc9064354d06a
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-03-16 20:36:35 +0100

    add more plots

--- src/implementation.py
+++ src/implementation.py
@@ -7,13 +7,14 @@ from sklearn.metrics import confusion_matrix
 import itertools
 from keras.utils.np_utils import to_categorical 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization, PReLU
 from keras.optimizers import RMSprop, Adam
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+import tensorflow as tf
 
 
-def dataPreprocessing():
+def data_Preprocessing():
     # load the data
     train = pd.read_csv("data/train.csv")
     test = pd.read_csv("data/test.csv")
@@ -41,32 +42,39 @@ def dataPreprocessing():
     X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=2)
     return Y_train,X_train,X_val,Y_val,X_test
 
-def modelGenerator():
+def model_Generator():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)))
+    model.add(Conv2D(32, (3, 3),  input_shape=(28, 28, 1)))
+    model.add(PReLU())
     model.add(BatchNormalization())
-    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(PReLU())
     model.add(BatchNormalization())
-    model.add(Conv2D(32, (5, 5), strides=2, padding='same', activation="relu"))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.4))
-
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(BatchNormalization())
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(BatchNormalization())
-    model.add(Conv2D(64, (5, 5), strides=2, padding='same', activation="relu"))
+    model.add(Conv2D(32, (5, 5), strides=2, padding='same'))
+    model.add(PReLU())
     model.add(BatchNormalization())
     model.add(Dropout(0.4))
 
-    model.add(Conv2D(128, (4, 4), activation="relu"))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3)))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (5, 5), strides=2, padding='same'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(128, (4, 4)))
+    model.add(PReLU())
     model.add(BatchNormalization())
     model.add(Flatten())
     model.add(Dropout(0.4))
-    model.add(Dense(10, activation="softmax"))
+    model.add(Dense(10, activation="softmax", kernel_regularizer= tf.keras.regularizers.l2(0.01)))
     return model
 
-def plotLossAcc(history):
+def plot_Loss_Acc(history):
     # Plot the loss and accuracy curves for training and validation 
     fig, ax = plt.subplots(2,1)
     ax[0].plot(history.history['loss'], color='b', label="Training loss")
@@ -78,6 +86,7 @@ def plotLossAcc(history):
     legend = ax[1].legend(loc='best', shadow=True)
     # fig.show()
     plt.savefig('loss_accuracy.png')
+    plt.close()
 
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -87,7 +96,6 @@ def plot_confusion_matrix(cm, classes,
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
-    plt.close()
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -109,9 +117,26 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
     # plt.show()
     plt.savefig('confusion_matrix.png')
+    plt.close()
 
+def plot_errors(errors_index,img_errors,pred_errors, obs_errors):
+    """ This function shows 6 images with their predicted and real labels"""
+    n = 0
+    nrows = 2
+    ncols = 3
+    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
+    for row in range(nrows):
+        for col in range(ncols):
+            error = errors_index[n]
+            ax[row,col].imshow((img_errors[error]).reshape((28,28)))
+            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
+            n += 1
+    
+    # plt.show()
+    plt.savefig("errors.png")
+    plt.close()
 
-Y_train, X_train, X_val, Y_val,X_test = dataPreprocessing()
+Y_train, X_train, X_val, Y_val,X_test = data_Preprocessing()
 # Data Augmentation
 datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
@@ -125,14 +150,12 @@ datagen = ImageDataGenerator(
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=False,  # randomly flip images
         vertical_flip=False)  # randomly flip images
-model = modelGenerator()
+
+model = model_Generator()
 
 # initial
 epochs = 2
 batch_size = 32
-
-# Define the optimizer
-# optimizer = RMSprop(learning_rate=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
 # Compile the model
 model.compile(optimizer = 'adam' , loss = "categorical_crossentropy", metrics=["accuracy"])
@@ -155,13 +178,7 @@ history = model.fit(datagen.flow(X_train,Y_train, batch_size=batch_size),
 _, accuracy = model.evaluate(X_val,Y_val)
 print(f"Validation Accuracy: {accuracy}")
 
-
-
-
-plotLossAcc(history)
-
-# Look at confusion matrix 
-
+plot_Loss_Acc(history)
 
 # Predict the values from the validation dataset
 Y_pred = model.predict(X_val)
@@ -174,46 +191,33 @@ confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
 # plot the confusion matrix
 plot_confusion_matrix(confusion_mtx, classes = range(10)) 
 
-# # Display some error results 
+# Display some error results 
 
-# # Errors are difference between predicted labels and true labels
-# errors = (Y_pred_classes - Y_true != 0)
+# Errors are difference between predicted labels and true labels
+errors = (Y_pred_classes - Y_true != 0)
 
-# Y_pred_classes_errors = Y_pred_classes[errors]
-# Y_pred_errors = Y_pred[errors]
-# Y_true_errors = Y_true[errors]
-# X_val_errors = X_val[errors]
+Y_pred_classes_errors = Y_pred_classes[errors]
+Y_pred_errors = Y_pred[errors]
+Y_true_errors = Y_true[errors]
+X_val_errors = X_val[errors]
 
-# def display_errors(errors_index,img_errors,pred_errors, obs_errors):
-#     """ This function shows 6 images with their predicted and real labels"""
-#     n = 0
-#     nrows = 2
-#     ncols = 3
-#     fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
-#     for row in range(nrows):
-#         for col in range(ncols):
-#             error = errors_index[n]
-#             ax[row,col].imshow((img_errors[error]).reshape((28,28)))
-#             ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
-#             n += 1
+# Probabilities of the wrong predicted numbers
+Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
 
-# # Probabilities of the wrong predicted numbers
-# Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+# Predicted probabilities of the true values in the error set
+true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
 
-# # Predicted probabilities of the true values in the error set
-# true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+# Difference between the probability of the predicted label and the true label
+delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
 
-# # Difference between the probability of the predicted label and the true label
-# delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+# Sorted list of the delta prob errors
+sorted_dela_errors = np.argsort(delta_pred_true_errors)
 
-# # Sorted list of the delta prob errors
-# sorted_dela_errors = np.argsort(delta_pred_true_errors)
+# Top 6 errors 
+most_important_errors = sorted_dela_errors[-6:]
 
-# # Top 6 errors 
-# most_important_errors = sorted_dela_errors[-6:]
-
-# # Show the top 6 errors
-# display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
+# Show the top 6 errors
+plot_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
 
 # predict results
 prediction = model.predict(X_test)
@@ -225,4 +229,4 @@ results = pd.Series(Y_pred,name="Label")
 
 submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
 
-submission.to_csv("cnn_mnist_datagen.csv",index=False)
+submission.to_csv("submission.csv",index=False)
