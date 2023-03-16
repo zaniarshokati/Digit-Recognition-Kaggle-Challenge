b220654990d4aac9c8db78a35b8abc372ff5b6ee
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-03-16 14:48:49 +0100

    fix few plot

--- src/implementation.py
+++ src/implementation.py
@@ -1,125 +1,83 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
 import seaborn as sns
-# # %matplotlib inline
-
-# np.random.seed(2)
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import itertools
-
 from keras.utils.np_utils import to_categorical 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 
-sns.set(style='white', context='notebook', palette='deep')
+def dataPreprocessing():
+    # load the data
+    train = pd.read_csv("data/train.csv")
+    test = pd.read_csv("data/test.csv")
 
-# load the data
-train = pd.read_csv("data/train.csv")
-test = pd.read_csv("data/test.csv")
+    # Drop 'label' column
+    X_train = train.drop(labels = ["label"],axis = 1)
+    Y_train = train["label"]
+    X_test = test.values
+    # free some space
+    del train 
 
-Y_train = train["label"]
-# Drop 'label' column
-X_train = train.drop(labels = ["label"],axis = 1) 
-# free some space
-del train 
+    # normalize the data
+    # reshape => images are 28x28, reshape in 28x28x1 3D matrix
+    # image as 3D with channels-last [rows][cols][channels]
+    # for gray scaled is just 1 channel
+    X_train = X_train.values.reshape(-1,28,28,1)/255.0
+    X_test = X_test.reshape(-1,28,28,1)/255.0
 
-# normalize the data
-X_train /= 255
-test /= 255
+    # convert to one-hot-encoding
+    # label encoding => one-hot vectors
+    # 4 => [0,0,0,0,1,0,0,0,0]
+    Y_train = to_categorical(Y_train, num_classes=10)
 
-# reshape => images are 28x28, reshape in 28x28x1 3D matrix
-# image as 3D with channels-last [rows][cols][channels]
-# for gray scaled is just 1 channel
-X_train = X_train.values.reshape(-1,28,28,1)
-test = test.values.reshape(-1,28,28,1)
+    # use 10% of data for testing an d90% fro training
+    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=2)
+    return Y_train,X_train,X_val,Y_val,X_test
 
-# convert to one-hot-encoding
-# label encoding => one-hot vectors
-# 4 => [0,0,0,0,1,0,0,0,0]
-Y_train = to_categorical(Y_train, num_classes=10)
+def modelGenerator():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (5, 5), strides=2, padding='same', activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
 
-# use 10% of data for testing an d90% fro training
-X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=2)
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (5, 5), strides=2, padding='same', activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
 
-# the model
-model = Sequential()
-model.add(Conv2D(filters = 8, kernel_size = (5,5),padding = 'Same', activation ='relu', input_shape = (28,28,1))) # 8 filters and kernel size=5,5
-# convolution with filters that transform the parts of the image
-model.add(MaxPool2D(pool_size=(2,2)))
-# looks at the 2 neighboring pixels and picks the maximal value
-# reduce computational costs and overfitting
-model.add(Dropout(0.25)) 
-# randomly selected neurons are ignored
+    model.add(Conv2D(128, (4, 4), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(10, activation="softmax"))
+    return model
 
-model.add(Conv2D(filters = 16, kernel_size = (3,3),padding = 'Same', activation ='relu')) # 16 filters and kernel size=3,3
-model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
-model.add(Dropout(0.25))
+def plotLossAcc(history):
+    # Plot the loss and accuracy curves for training and validation 
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(history.history['loss'], color='b', label="Training loss")
+    ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+    legend = ax[0].legend(loc='best', shadow=True)
 
-model.add(Flatten()) # 1D vector
-model.add(Dense(256, activation = "relu")) # fully connected
-model.add(Dropout(0.5))
-model.add(Dense(10, activation = "softmax")) # probability distribution over the 10 classes
-
-# initial
-epochs = 5
-batch_size = 80
-
-# Define the optimizer
-optimizer = RMSprop(learning_rate=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-
-# Compile the model
-model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
-
-# Reduce learning rate when a metric has stopped improving.
-learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
-                                            patience=3, 
-                                            verbose=1, 
-                                            factor=0.5, 
-                                            min_lr=0.00001)
-
-
-# With data augmentation to prevent overfitting (accuracy 0.99286)
-
-datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.1, # Randomly zoom image 
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=False,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-
-datagen.fit(X_train)
-
-# Fit the model
-history = model.fit(datagen.flow(X_train,Y_train, batch_size=batch_size),
-                              epochs = epochs, validation_data = (X_val,Y_val),
-                              verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
-                              , callbacks=[learning_rate_reduction])
-
-# Plot the loss and accuracy curves for training and validation 
-fig, ax = plt.subplots(2,1)
-ax[0].plot(history.history['loss'], color='b', label="Training loss")
-ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
-legend = ax[0].legend(loc='best', shadow=True)
-
-ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
-ax[1].plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
-legend = ax[1].legend(loc='best', shadow=True)
-
-# Look at confusion matrix 
+    ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
+    ax[1].plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+    legend = ax[1].legend(loc='best', shadow=True)
+    # fig.show()
+    plt.savefig('loss_accuracy.png')
 
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -129,6 +87,7 @@ def plot_confusion_matrix(cm, classes,
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
+    plt.close()
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -148,6 +107,61 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    # plt.show()
+    plt.savefig('confusion_matrix.png')
+
+
+Y_train, X_train, X_val, Y_val,X_test = dataPreprocessing()
+# Data Augmentation
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+model = modelGenerator()
+
+# initial
+epochs = 2
+batch_size = 32
+
+# Define the optimizer
+# optimizer = RMSprop(learning_rate=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+# Compile the model
+model.compile(optimizer = 'adam' , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+# Add early stopping, model checkpointing, and learning rate reduction
+callbacks = [
+    EarlyStopping(patience=10, restore_best_weights=True),
+    ModelCheckpoint("best_model.h5", save_best_only=True),
+    ReduceLROnPlateau(factor=0.5, patience=3, min_lr=0.00001, verbose=1)
+]
+
+datagen.fit(X_train)
+
+# Fit the model
+history = model.fit(datagen.flow(X_train,Y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (X_val,Y_val)
+                              , callbacks=callbacks)
+
+# Evaluate model on validation set
+_, accuracy = model.evaluate(X_val,Y_val)
+print(f"Validation Accuracy: {accuracy}")
+
+
+
+
+plotLossAcc(history)
+
+# Look at confusion matrix 
+
 
 # Predict the values from the validation dataset
 Y_pred = model.predict(X_val)
@@ -160,54 +174,54 @@ confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
 # plot the confusion matrix
 plot_confusion_matrix(confusion_mtx, classes = range(10)) 
 
-# Display some error results 
+# # Display some error results 
 
-# Errors are difference between predicted labels and true labels
-errors = (Y_pred_classes - Y_true != 0)
+# # Errors are difference between predicted labels and true labels
+# errors = (Y_pred_classes - Y_true != 0)
 
-Y_pred_classes_errors = Y_pred_classes[errors]
-Y_pred_errors = Y_pred[errors]
-Y_true_errors = Y_true[errors]
-X_val_errors = X_val[errors]
+# Y_pred_classes_errors = Y_pred_classes[errors]
+# Y_pred_errors = Y_pred[errors]
+# Y_true_errors = Y_true[errors]
+# X_val_errors = X_val[errors]
 
-def display_errors(errors_index,img_errors,pred_errors, obs_errors):
-    """ This function shows 6 images with their predicted and real labels"""
-    n = 0
-    nrows = 2
-    ncols = 3
-    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
-    for row in range(nrows):
-        for col in range(ncols):
-            error = errors_index[n]
-            ax[row,col].imshow((img_errors[error]).reshape((28,28)))
-            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
-            n += 1
+# def display_errors(errors_index,img_errors,pred_errors, obs_errors):
+#     """ This function shows 6 images with their predicted and real labels"""
+#     n = 0
+#     nrows = 2
+#     ncols = 3
+#     fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
+#     for row in range(nrows):
+#         for col in range(ncols):
+#             error = errors_index[n]
+#             ax[row,col].imshow((img_errors[error]).reshape((28,28)))
+#             ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
+#             n += 1
 
-# Probabilities of the wrong predicted numbers
-Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+# # Probabilities of the wrong predicted numbers
+# Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
 
-# Predicted probabilities of the true values in the error set
-true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+# # Predicted probabilities of the true values in the error set
+# true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
 
-# Difference between the probability of the predicted label and the true label
-delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+# # Difference between the probability of the predicted label and the true label
+# delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
 
-# Sorted list of the delta prob errors
-sorted_dela_errors = np.argsort(delta_pred_true_errors)
+# # Sorted list of the delta prob errors
+# sorted_dela_errors = np.argsort(delta_pred_true_errors)
 
-# Top 6 errors 
-most_important_errors = sorted_dela_errors[-6:]
+# # Top 6 errors 
+# most_important_errors = sorted_dela_errors[-6:]
 
-# Show the top 6 errors
-display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
+# # Show the top 6 errors
+# display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
 
 # predict results
-results = model.predict(test)
+prediction = model.predict(X_test)
 
 # select the index with the maximum probability
-results = np.argmax(results,axis = 1)
+Y_pred = np.argmax(prediction,axis = 1)
 
-results = pd.Series(results,name="Label")
+results = pd.Series(Y_pred,name="Label")
 
 submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
 
